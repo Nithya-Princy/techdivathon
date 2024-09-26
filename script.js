424c17eed73d082016eba703abe3b2e305ffc2d3
Nithya-Princy <nithyangelinap8@gmail.com>
2024-09-26 16:00:21 +0530

    Techdivathon

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+// Example JavaScript code to display a message on page load
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('TechDivathon Dashboard is ready!');
+});
